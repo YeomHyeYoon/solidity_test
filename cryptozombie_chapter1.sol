73668dd0a316e397bf724bd0be903088b37e2fe2
Yuni <hyeyoon5019@naver.com>
2021-07-29 16:44:38 +0900

    add crytozombie chapter1

---
+++ cryptozombie_chapter1.sol
@@ -0,0 +1,33 @@
+//솔리디티 버전 지정
+pragma solidity >=0.7.0 <0.9.0;
+
+contract ZombieFactory {
+
+    // 여기에 이벤트 선언
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function _createZombie(string memory _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+        // 여기서 이벤트 실행
+    }
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
+}
