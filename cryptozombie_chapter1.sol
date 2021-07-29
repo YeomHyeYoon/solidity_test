cb13eb62a78a58b3cf5f47556ef64c6274cf526b
Yuni <hyeyoon5019@naver.com>
2021-07-29 16:57:08 +0900

    modify capter1

--- cryptozombie_chapter1.sol
+++ cryptozombie_chapter1.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract ZombieFactory {
 
-    // 여기에 이벤트 선언
+    event NewZombie(uint zombieId , string name , uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
@@ -17,7 +17,9 @@ contract ZombieFactory {
 
     function _createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
-        // 여기서 이벤트 실행
+        uint id = zombies.length - 1;
+        //이벤트 실행
+        emit NewZombie(id, _name, _dna);
     }
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
